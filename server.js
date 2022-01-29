ddb3f506177f12a7d7c35d99fd4d2ba15fb9d4c8
anmrez <zzirg1@mail.ru>
2022-01-29 17:46:42 +0300

    update v0.13
    - removed "routing" from "server.js"
    - added "routing.js"
    - added partials (handlebars)
    - added connect mongoose
    - added data output in /get-cookie from mongoose

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const express = require('express'),
   exphbs = require('express-handlebars'),
   bodyParser = require('body-parser'),
   session = require('express-session'),
-  logging = require('./lib/logging.js')
-  // mongo = require('mongodb').MongoClient
-  // redisStorage = require('connect-redis')(session)
-  // redis = require('redis'),
-  // client = redis.createClient();
+  routes = require('./lib/routing.js'),
+  mongoose = require('mongoose')
+    // redisStorage = require('connect-redis')(session)
+    // redis = require('redis'),
+    // client = redis.createClient();
 
 
 // setting
@@ -50,72 +50,24 @@ app.use(
 )
 
 
-
 // END session
-// routing  ========================
+
+app.use(routes)
 
 
-// get-cookie page
-app.get('/get-cookie', (req, res) => {
-  res.render('get__cookie',{
-    cookieName: req.cookies.token,
-  })
-})
 
 
-// login page
-app.get('/login', (req, res) => {
-  res.render('login',{
-    cookieName: req.cookies.token
-  })
-})
-
-// ответ из формы
-// создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = express.urlencoded({extended: false});
-app.post("/login", urlencodedParser, function (req, res) {
-  if(!req.body) return res.sendStatus(400);
-  console.log(req.body);
-  if (!req.cookies.token) { // если нет куки 'token' то создай
-    console.log('cookie "token"= undefined, create "token"');
-
-    // создание куки с значением в инпуте userName
-    res.cookie('token', `${req.body.userName}`, {
-      maxAge: 1000 * 60 * 60 * 24, // устнавливаеться 'жизнь' (24ч.)
-      // signed: true
-    });
-
-    console.log(`token create: '${req.body.userName}'`)
-  };
-
-  res.redirect("/") // Перенаправить на домашнюю страницу
-}); // app.post "/login"
-// END login page
-
-
-// home page
-app.get('/', (req, res) => {
-  res.render('home',{
-    cookieName: req.cookies.token,
-  }) // render 'home'
-}) // app.get '/'
-
-
-// page 404
-app.use(function(req, response){
-  response.status(404);
-  response.render('404', { layout: '404' });
-});
-
-
-// END routing
-// listen
-
-
-app.listen(app.get('port'), function(){
-  console.log(`server started in http://127.0.0.1:${app.get('port')} Stop in CTRL + C`);
-    console.log(`------------------------------------------------------`);
-})
-
-
-// END listen
+async function start(){
+  try {
+    await mongoose.connect('', {
+      useNewUrlParser: true
+    })
+    app.listen(app.get('port'), function(){
+      console.log(`server started in http://127.0.0.1:${app.get('port')} Stop in CTRL + C`);
+        console.log(`------------------------------------------------------`);
+      })
+  } catch (e) {
+    console.log(e);
+  }
+}
+start()
