184abb3accd26bd9f8d26e95f164e65523ad89e1
anmrez <zzirg1@mail.ru>
2022-02-02 19:23:26 +0300

    update v0.17
    - added validation (in /login)
    - broken validation (in /registration)

--- views/assets/js/valid.js
+++ views/assets/js/valid.js
@@ -1,13 +1,21 @@
 const inputName = document.getElementById('inputName')
 const validName = document.getElementById('validName')
+const error = document.getElementById('symbolError')
+let str
+let a = []
+
 
 inputName.addEventListener("input", function (event) {
-  if (inputName.validity.patternMismatch) {
-    if (inputName.textLength > 1) {
+  str = inputName.value.toString().split('')
+
+  console.log(str[str.length-1]);
+  if (!/[a-zA-Z0-9_]/.test(str[str.length-1])) {
+    console.log(`invalid`);
+    a[a.length] = str[str.length-1]
+    if (a.length > -1) {
+      error.innerHTML = `${a}`
       validName.style.display = 'flex'
     }
-  } else {
-    validName.style.display = 'none'
   }
-  // console.log(inputName.value, inputName.validity.patternMismatch);
+  console.log(`a= ${a}`);
 });
