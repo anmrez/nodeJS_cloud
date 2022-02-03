7c1e9cea808ed5f7eff124ef5b967ea864e8c591
anmrez <zzirg1@mail.ru>
2022-02-03 18:40:25 +0300

    update v0.18
    - validation fixed (in /registration)

--- views/assets/js/valid.js
+++ views/assets/js/valid.js
@@ -1,21 +1,48 @@
 const inputName = document.getElementById('inputName')
 const validName = document.getElementById('validName')
 const error = document.getElementById('symbolError')
+const btnSubmit = document.getElementById('submit')
 let str
 let a = []
 
 
 inputName.addEventListener("input", function (event) {
   str = inputName.value.toString().split('')
-
-  console.log(str[str.length-1]);
+  console.log(btnSubmit);
   if (!/[a-zA-Z0-9_]/.test(str[str.length-1])) {
-    console.log(`invalid`);
-    a[a.length] = str[str.length-1]
-    if (a.length > -1) {
+    btnSubmit.disabled = true
+    // запись в 'А' нового символа если такого нет
+    if (!a.includes(str[str.length-1])) {
+      a[a.length] = str[str.length-1]
+    }
+
+    forA(a, str)
+
+    // вывод ошибки пользователю
+    if (a.length) {
       error.innerHTML = `${a}`
       validName.style.display = 'flex'
     }
-  }
-  console.log(`a= ${a}`);
+
+  } else {
+    btnSubmit.disabled = false
+    forA(a, str)
+    if (a == 0) validName.style.display = 'none'
+
+  } // if (!/[a-zA-Z0-9_]/
+
+  // console.log(`arr str: '${str}'  || a: '${a}' || a.length: '${a.length}'`);
 });
+
+
+function forA(a, str) {
+
+  for (var i = -1; i < a.length; i++) {
+
+    if (!str.includes(a[i+1])) { // если элемент из 'А' не найден в строке, тогда удалить эл. из А
+      a.splice(i+1, 1)
+    } // if
+
+  } // for
+
+} // forA(a, str)
